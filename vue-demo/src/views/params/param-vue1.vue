1dabd1f68d2bd2c68a9914f67463257e0e28f6f6
dev-dingxue <2656078066@qq.com>
2023-09-21 19:37:03 +0800

    最后一次提交

--- vue-demo/src/views/params/param-vue1.vue
+++ vue-demo/src/views/params/param-vue1.vue
@@ -14,7 +14,25 @@ import { inject } from 'vue';
 const _name = inject("message");
 
 const xiondi = () => {
+    fetch("../../../public/data/test.json").then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        return res;
+    }).then(data => {
+        console.log(data);
+    })
+}
 
+const api = async () => {
+    const respone = await fetch("", {
+        method: "get",
+        headers: {
+            "": "",
+        }
+    }).then(resp => resp.json()).then(data => data);
+
+    console.log(respone);
 }
 
 </script>
